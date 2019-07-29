c7972f5fabaffe0c08d005e165090a683e1efd1c
baxiyi <mzw5810@gmail.com>
2019-07-29 18:48:13 +0800

    Updates

---
+++ precache-manifest.132a25f9b166e73f2d92f9b999f6ed2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd3bd2b660d7aac7006eb8bad204eaa",
+    "url": "/react-router-test/index.html"
+  },
+  {
+    "revision": "9b07e1c0155fc85f04eb",
+    "url": "/react-router-test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0f3c66b36aa427114937",
+    "url": "/react-router-test/static/js/2.96045618.chunk.js"
+  },
+  {
+    "revision": "9b07e1c0155fc85f04eb",
+    "url": "/react-router-test/static/js/main.24497a15.chunk.js"
+  },
+  {
+    "revision": "8b9faef7b1cfceaaf400",
+    "url": "/react-router-test/static/js/runtime~main.05ad894c.js"
+  }
+]);
